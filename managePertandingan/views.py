6531722fb039b2ca3d9e29498a79339f0ab2c8b0
vinsensius.ferdinando <vinsensius.ferdinando@ui.ac.id>
2023-05-23 11:16:44 +0700

    Vinsen/ Fizing the navbar for Peminjaman Stadium, Mulai Rapat, Mulai Pertandingan, Manage Pertandingan

--- managePertandingan/views.py
+++ managePertandingan/views.py
@@ -3,6 +3,54 @@ import psycopg2
 from django.shortcuts import render
 
 # Create your views here.
+def get_role_context(role):
+    context = {
+        'role': role,
+        'login_status': 'hidden',
+        'register_status': 'hidden',
+        'dashboard_status': None,
+        'mengelola_tim_status': None,
+        'peminjaman_stadium_status': None,
+        'manage_pertandingan_status': None,
+        'pembelian_tiket_status': None,
+        'list_pertandingan_status': None,
+        'rapat_status': None,
+        'history_rapat_status': None,
+        'pembuatan_pertandingan_status': None,
+        'mulai_pertandingan_status': None
+    }
+
+    if role == 'manajer':
+        context.update({
+            'manage_pertandingan_status': 'hidden',
+            'pembelian_tiket_status': 'hidden',
+            'rapat_status': 'hidden',
+            'pembuatan_pertandingan_status': 'hidden',
+            'mulai_pertandingan_status': 'hidden'
+        })
+    elif role == 'penonton':
+        context.update({
+            'mengelola_tim_status': 'hidden',
+            'peminjaman_stadium_status': 'hidden',
+            'manage_pertandingan_status': 'hidden',
+            'rapat_status': 'hidden',
+            'history_rapat_status': 'hidden',
+            'pembuatan_pertandingan_status': 'hidden',
+            'mulai_pertandingan_status': 'hidden'
+        })
+    elif role == 'panitia':
+        context.update({
+            'mengelola_tim_status': 'hidden',
+            'peminjaman_stadium_status': 'hidden',
+            'pembelian_tiket_status': 'hidden',
+            'list_pertandingan_status': 'hidden',
+            'history_rapat_status': 'hidden',
+            'pembuatan_pertandingan_status': 'hidden',
+            'mulai_pertandingan_status': 'hidden'
+        })
+
+    return context
+
 def index(request):
     db_config = settings.DATABASES['default']
 
@@ -20,9 +68,11 @@ def index(request):
     query = f"SELECT * FROM {schema_name}.{table_name} WHERE username = %s AND password = %s;"
     # params = [username, password]
 
-
-
-    return render(request, 'manage.html')
+    context = {}
+    context.update(get_role_context('panitia'))
+    return render(request, 'manage.html',context)
 
 def peristiwa(request):
-    return render(request, 'peristiwa.html')
+    context = {}
+    context.update(get_role_context('panitia'))
+    return render(request, 'peristiwa.html',context)
