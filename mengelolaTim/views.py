b186e749d0e5f7916b77548f6c80dca5d4414187
alifilhmm <alifilhmm@gmail.com>
2023-05-23 20:33:28 +0700

    alif/try

--- mengelolaTim/views.py
+++ mengelolaTim/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+from django.conf import settings
+import psycopg2
 
 def index(request):
+    db_config = settings.DATABASES['default']
+
+    conn = psycopg2.connect(
+        dbname=db_config['NAME'],
+        user=db_config['USER'],
+        password=db_config['PASSWORD'],
+        host=db_config['HOST'],
+        port=db_config['PORT']
+    )
+    cur = conn.cursor()
+
+    schema_name = 'sepak_bola'
+
 
     return render(request, 'mengelola.html')
 
 def register_team(request):
+    db_config = settings.DATABASES['default']
+
+    conn = psycopg2.connect(
+        dbname=db_config['NAME'],
+        user=db_config['USER'],
+        password=db_config['PASSWORD'],
+        host=db_config['HOST'],
+        port=db_config['PORT']
+    )
+    cur = conn.cursor()
+
+    schema_name = 'sepak_bola'
+    
 
     return render(request, 'register_team.html')
 
